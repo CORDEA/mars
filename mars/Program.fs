98320d03b15d1fe99154482617c2e7bd9e42b0c1
Yoshihiro Tanaka <contact@cordea.jp>
2022-04-09 15:30:15 +0900

    build url

--- mars/Program.fs
+++ mars/Program.fs
@@ -1,2 +1,27 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+
+let Host = "api.instagram.com"
+let AuthorizePath = "oauth/authorize"
+let AccessTokenPath = "oauth/access_token"
+let RedirectUri = "https://localhost"
+
+[<EntryPoint>]
+let main argv =
+    let clientId = argv[0]
+
+    let builder = UriBuilder("https", Host)
+    builder.Path <- AuthorizePath
+
+    let query =
+        [ ("client_id", clientId)
+          ("redirect_uri", RedirectUri)
+          ("scope", "user_profile,user_media")
+          ("response_type", "code") ]
+        |> List.map (fun (k, v) -> $"%s{k}=%s{v}")
+        |> String.concat "&"
+
+    builder.Query <- query
+
+    printfn $"{builder}"
+
+    0
