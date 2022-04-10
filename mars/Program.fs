a4aeaca30f624b281a996e069371b7640fc803da
Yoshihiro Tanaka <contact@cordea.jp>
2022-04-10 10:38:33 +0900

    request an access token

--- mars/Program.fs
+++ mars/Program.fs
@@ -1,16 +1,18 @@
 ﻿open System
+open System.Net.Http
 
 let Host = "api.instagram.com"
 let AuthorizePath = "oauth/authorize"
 let AccessTokenPath = "oauth/access_token"
-let RedirectUri = "https://localhost"
+let RedirectUri = "https://localhost/"
 
 [<EntryPoint>]
 let main argv =
     let clientId = argv[0]
+    let clientSecret = argv[1]
 
-    let builder = UriBuilder("https", Host)
-    builder.Path <- AuthorizePath
+    let authorizeUrl = UriBuilder("https", Host)
+    authorizeUrl.Path <- AuthorizePath
 
     let query =
         [ ("client_id", clientId)
@@ -20,8 +22,28 @@ let main argv =
         |> List.map (fun (k, v) -> $"%s{k}=%s{v}")
         |> String.concat "&"
 
-    builder.Query <- query
+    authorizeUrl.Query <- query
 
-    printfn $"{builder}"
+    printfn $"{authorizeUrl}"
+
+    let code = Console.ReadLine()
+    let request = new MultipartFormDataContent()
+
+    [ ("client_id", clientId)
+      ("client_secret", clientSecret)
+      ("grant_type", "authorization_code")
+      ("redirect_uri", RedirectUri)
+      ("code", code) ]
+    |> List.map (fun (k, v) -> (new StringContent(v), k))
+    |> List.iter request.Add
+
+    let client = new HttpClient()
+    let accessTokenUrl = UriBuilder("https", Host)
+    accessTokenUrl.Path <- AccessTokenPath
+
+    let response =
+        client.PostAsync(accessTokenUrl.ToString(), request)
+        |> Async.AwaitTask
+        |> Async.RunSynchronously
 
     0
