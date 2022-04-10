87e9dec31d5a30aceb5271a35631927b3d8d7fea
Yoshihiro Tanaka <contact@cordea.jp>
2022-04-10 11:38:20 +0900

    fetch user information

--- mars/Program.fs
+++ mars/Program.fs
@@ -2,29 +2,36 @@
 open System.IO
 open System.Net.Http
 open FSharp.Data
+open FSharpx.Control
 
-let Host = "api.instagram.com"
+let AuthorizeHost = "api.instagram.com"
 let AuthorizePath = "oauth/authorize"
 let AccessTokenPath = "oauth/access_token"
 let RedirectUri = "https://localhost/"
+let GraphApiHost = "graph.instagram.com"
+let UserPath = "me"
+
+let buildQuery (values: (string * string) list) : string =
+    values
+    |> List.map (fun (k, v) -> $"%s{k}=%s{v}")
+    |> String.concat "&"
 
 [<EntryPoint>]
 let main argv =
     let clientId = argv[0]
     let clientSecret = argv[1]
 
-    let authorizeUrl = UriBuilder("https", Host)
+    let authorizeUrl = UriBuilder("https", AuthorizeHost)
     authorizeUrl.Path <- AuthorizePath
 
-    let query =
+    let authorizeQuery =
         [ ("client_id", clientId)
           ("redirect_uri", RedirectUri)
           ("scope", "user_profile,user_media")
           ("response_type", "code") ]
-        |> List.map (fun (k, v) -> $"%s{k}=%s{v}")
-        |> String.concat "&"
+        |> buildQuery
 
-    authorizeUrl.Query <- query
+    authorizeUrl.Query <- authorizeQuery
 
     printfn $"{authorizeUrl}"
 
@@ -40,7 +47,7 @@ let main argv =
     |> List.iter request.Add
 
     let client = new HttpClient()
-    let accessTokenUrl = UriBuilder("https", Host)
+    let accessTokenUrl = UriBuilder("https", AuthorizeHost)
     accessTokenUrl.Path <- AccessTokenPath
 
     let response =
@@ -61,6 +68,31 @@ let main argv =
             | None -> raise (InvalidDataException "Access token is not included.")
             | Some token -> token.AsString())
 
-    printfn $"{token}"
+
+    let meUrl =
+        UriBuilder(Uri.UriSchemeHttps, GraphApiHost)
+
+    meUrl.Path <- UserPath
+
+    meUrl.Query <-
+        [ ("fields", "username")
+          ("access_token", token) ]
+        |> buildQuery
+
+    let user =
+        client.GetAsync(meUrl.ToString())
+        |> Async.AwaitTask
+        |> Async.bind (fun v -> v.Content.ReadAsStringAsync() |> Async.AwaitTask)
+        |> Async.map JsonValue.Parse
+        |> Async.RunSynchronously
+
+    let name =
+        user.TryGetProperty("username")
+        |> (fun v ->
+            match v with
+            | None -> raise (InvalidDataException "Username is not included.")
+            | Some name -> name.AsString())
+
+    printfn $"Hello {name}!"
 
     0
